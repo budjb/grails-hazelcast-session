f4568f00093fb0dce6a6fa78fd92fc291c6dffc0
Bud Byrd <bud.byrd@gmail.com>
2017-09-07 15:21:21 -0500

    Add null checks for missing session in the flash interceptor.

--- grails-app/controllers/com/budjb/hazelcast/session/FlashInterceptor.groovy
+++ grails-app/controllers/com/budjb/hazelcast/session/FlashInterceptor.groovy
@@ -4,12 +4,21 @@ import grails.artefact.Interceptor
 import grails.web.mvc.FlashScope
 import org.grails.web.servlet.mvc.GrailsWebRequest
 import org.grails.web.util.GrailsApplicationAttributes
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.context.request.RequestContextHolder
+
+import javax.servlet.http.HttpSession
 
 /**
  * An interceptor that ensures that the flash scope is properly stored in the session.
  */
 class FlashInterceptor implements Interceptor {
+    /**
+     * Logger.
+     */
+    Logger log = LoggerFactory.getLogger(getClass())
+
     /**
      * Match all requests.
      */
@@ -21,8 +30,18 @@ class FlashInterceptor implements Interceptor {
      * After the view has finished rendering, grab the flash scope data and store it in the request's session.
      */
     void afterView() {
-        GrailsWebRequest webRequest = (GrailsWebRequest) RequestContextHolder.currentRequestAttributes()
-        FlashScope flashScope = webRequest.getFlashScope()
-        request.getSession().setAttribute(GrailsApplicationAttributes.FLASH_SCOPE, flashScope)
+        FlashScope flashScope = ((GrailsWebRequest) RequestContextHolder.currentRequestAttributes()).getFlashScope()
+
+        if (flashScope.isEmpty()) {
+            return
+        }
+
+        HttpSession session = request.getSession()
+        if (session != null) {
+            session.setAttribute(GrailsApplicationAttributes.FLASH_SCOPE, flashScope)
+        }
+        else {
+            log.warn("unable to store flash scope in the session because the session is null")
+        }
     }
 }
